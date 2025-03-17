5a21670e3942b9e5f197974e296fc700a169e53a
DavidSosa <davidgsosa1@gmail.com>
2025-03-17 11:45:27 -0700

    backend now complete. Refactor and more done

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,8 +2,12 @@ import dotenv from "dotenv";
 dotenv.config();
 import foodRouter from "./routers/food.router.js";
 import userRouter from "./routers/user.router.js";
+import orderRouter from "./routers/order.router.js";
+import uploadRouter from "./routers/upload.router.js";
 import cors from "cors";
 import express from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 import { connectDB } from "./config/database.config.js";
 
 connectDB();
@@ -17,6 +21,16 @@ app.use(cors({ credentials: true, origin: ["http://localhost:3000"] }));
 
 app.use("/api/foods", foodRouter);
 app.use("/api/users", userRouter);
+app.use("/api/orders", orderRouter);
+app.use("/api/upload", uploadRouter);
+
+const publicFolder = path.join(__dirname, "public");
+app.use(express.static(publicFolder));
+
+app.get("*", (req, res) => {
+    const indexFilePath = path.join(publicFolder, "index.html");
+    res.sendFile(indexFilePath);
+});
 
 // listening on port 5001
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
