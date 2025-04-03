0f2478bd31f0bb66d6f06fbe4d48b0621b57f275
DavidSosa <davidgsosa1@gmail.com>
2025-04-03 09:23:52 -0700

    Added tags feature

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./components/navbar/Navbar";
 import AppRoutes from "./AppRoutes";
+import { useLoading } from "./hooks/useLoading";
+import { setLoadingInterceptor } from "./interceptors/loadingInterceptors";
+import Loading from "./components/loading/Loading";
 
 function App() {
+    const { showLoading, hideLoading } = useLoading();
+
+    useEffect(() => {
+        setLoadingInterceptor({ showLoading, hideLoading });
+    }, []);
+
     return (
         <>
+            <Loading />
             <Navbar />
             <AppRoutes />
         </>
---
+++ frontend/src/components/tags/Tag.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import classes from "./tags/module.css";
+
+export default function Tags({ tags, forFoodPage }) {
+    return (
+        <>
+            <div></div>
+        </>
+    );
+}
